68e47ea0576d89347cd87be9d1d2406ee6de92ff
Mauro George <maurogot@gmail.com>
2010-05-05 15:56:45 -0300

    Adicionado o doctest

--- exercicios/livro/parte-4/1.py
+++ exercicios/livro/parte-4/1.py
@@ -4,23 +4,33 @@
 1. Crie uma classe que modele um quadrado
 com um atributo lado e os métodos: mudar valor do lado,
 retornar valor do lado e calcular área.
+
+>>> quadrado = Quadrado()
+>>> quadrado.set_lado(7)
+>>> quadrado.lado
+7
+
+>>> quadrado.get_lado()
+7
+
+>>> quadrado.area()
+49
 """
 
 class Quadrado(object):
 	
 	
-	def setLado(self, lado):
+	def set_lado(self, lado):
 		self.lado = lado
 	
-	def getLado(self):
+	def get_lado(self):
 		return self.lado
 		
 	def area(self):
 		return self.lado**2	
 	
 
-# Executando	
-quadrado = Quadrado()
-quadrado.setLado(7)
-print quadrado.getLado()
-print quadrado.area()
+
+if __name__ == "__main__":
+	import doctest
+	doctest.testmod()
