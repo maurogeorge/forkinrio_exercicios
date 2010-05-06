fef4f62c7b292351de17b9b2e847999d9de682ab
maurogeorge <maurogot@gmail.com>
2010-05-05 21:29:41 -0300

    Resolvido o problema ao conecetar no github, e adicionado o Doctest no capitulo 4 exercicio 2

--- exercicios/livro/parte-4/2.py
+++ exercicios/livro/parte-4/2.py
@@ -4,6 +4,9 @@
 2. Crie uma classe derivada de lista
 com um método retorne os elementos da lista
 sem repetição
+
+>>> Lista([1,2,2,3]) - [3,4,5,6]
+[1, 2, 3, 4, 5, 6]
 """
 
 class Lista(list):
@@ -19,7 +22,8 @@ class Lista(list):
 				retorno.append(i)
 		
 		return retorno			
+	
 
-# Executando		
-print Lista([1,2,2,3]) - [3,4,5,6]	
-
+if __name__ == "__main__":
+	import doctest
+	doctest.testmod()
