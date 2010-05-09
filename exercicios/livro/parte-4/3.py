6018b77df2b8a16629553218158de1f8a122e99f
maurogeorge <maurogot@gmail.com>
2010-05-09 09:15:15 -0300

    Adicionado o unittest no exercicio 4.3

--- exercicios/livro/parte-4/3.py
+++ exercicios/livro/parte-4/3.py
@@ -31,10 +31,34 @@ class Carro(object):
 	def abastecer(self, combustivel):
 		self.combustivel += combustivel
 
-
 # Executando
-carrinho = Carro(10)
-carrinho.abastecer(100)
-carrinho.mover(3)
-print carrinho.gasolina()			
+def main():
+	carrinho = Carro(10)
+	carrinho.abastecer(100)
+	carrinho.mover(3)
+	print carrinho.gasolina()			
+
+import unittest
+
+class TestCarro(unittest.TestCase):
+	
+	def setUp(self):
+		self.carro = Carro(10)
+
+		
+	def test_verifica_se_abasteceu_100(self):
+		self.carro.abastecer(100)
+		abastece = 100
+		self.assertEquals(abastece, self.carro.gasolina())	
+		
+	def test_move_3_resta_70_de_gasolina(self):
+		self.carro.abastecer(100)
+		move = 3
+		self.carro.mover(3)
+		self.assertEquals(70, self.carro.gasolina())
+
+if __name__ == '__main__':
+	main()
+	unittest.main()
+			
 	
