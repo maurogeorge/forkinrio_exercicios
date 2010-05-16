a399e734e1f67375aaabe891af699db2ba963405
maurogeorge <maurogot@gmail.com>
2010-05-16 10:06:58 -0300

    Exercicio 5.2 criando uma tabela mais semântica

--- exercicios/livro/parte-5/2.py
+++ exercicios/livro/parte-5/2.py
@@ -1,7 +1,7 @@
 # -*- coding: utf8 -*-
 
 """
-1. Implementar uma função que formate uma lista de tuplas como
+2. Implementar uma função que formate uma lista de tuplas como
 tabela HTML
 """
 
@@ -10,19 +10,35 @@ def tabela_html(lista):
 	# Cria a tabela
 	retorno = '<table> \n'
 	
-	# Cada tupla na lista representa uma linha
-	for tupla in lista:
+	# Cria o head da tabela
+	retorno += '\t<thead>\n'
+	retorno += '\t\t<tr>\n'
+	
+	# lê a primeira tupla, e cria o head
+	for dado in lista[0]:
+		retorno += '\t\t\t<th>%s</th>\n' %(dado)
+	
+	# Finaliza o head	
+	retorno += '\t\t</tr>\n'
+	retorno += '\t</thead>\n'
+	
+	# Cria o corpo da tabela
+	retorno += '\t<tbody>\n'
+	
+	# Cada tupla na lista representa uma linha começando da segunda pois a primeira é o head
+	for tupla in lista[1:]:
 		# Cria a linha
-		retorno += '\t <tr> \n'
+		retorno += '\t\t<tr> \n'
 		
 		# lê os dados da tupla	
 		for dado in tupla:
-			retorno += '\t \t<td>%s</td>\n' %(dado)
+			retorno += '\t\t\t<td>%s</td>\n' %(dado)
 			
 		# Termina a linha
-		retorno += '\t</tr>\n'	
+		retorno += '\t\t</tr>\n'	
 
 	# Finaliza a tabela
+	retorno += '\t</tbody>\n'
 	retorno += '</table>'
 	
 	# Grava os dados em arquivo.html
